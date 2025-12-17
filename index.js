4f5da42d5554e34a4cbc2c2d0b88b6503d741f77
ujin22 <p.b.m.aubert@gmail.com>
2025-12-17 16:42:34 +0100

    Install express and nodemon for local development, index html is still to large

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-//TODO: 
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use('/', express.static(path.join(__dirname, 'app')));
 
 
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
 
 /***
- * run express server
- * local_host?! 
  * ENDPOINTS: 
  * 
  * 
