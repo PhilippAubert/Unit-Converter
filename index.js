e8705831c9b0089fd347671a5288f1bd994316b7
ujin22 <p.b.m.aubert@gmail.com>
2025-12-19 02:24:37 +0100

    CLean up frontend (more ideas to come), add post request, add simple response html code.

--- index.js
+++ index.js
@@ -9,9 +9,32 @@ const __dirname = path.dirname(__filename);
 app.use('/', express.static(path.join(__dirname, 'app')));
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    console.log('POST request received');
+    const { length, from, to } = req.body;
+
+    //CALCULATION STARTS HERE! 
+    // SEND SIMPLIFED INDEX WITH H1 AS TEMPLATE LITERAL 
+    // ERROR HANDLING GALORE!!! 
+
+    res.send(`
+        <h1>Received value</h1>
+        <p>Length: ${length}</p>
+        <a href="/">Back</a>
+    `);    
+});
+
+
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
+
+//temperature=5645&to=celsius&to=fahrenheit
+
+
 
 /***
  * ENDPOINTS: 
